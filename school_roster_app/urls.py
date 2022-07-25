c12879b924f1bbc6332b8363b995fe7a01b2113b
Michael Echevarria <michael.echevarria@live.com>
2022-07-24 23:24:34 -0500

    last push

---
+++ school_roster_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("staff/", views.list_staff, name="list-staff"),
+    path("staff/<int:employee_id>/", views.staff_detail, name="staff-detail"),
+    path("students/", views.list_students, name="list-students"),
+    path("students/<int:student_id>/", views.student_detail, name="student-detail"),
+]
---
+++ school_roster_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .models import School, Student, Staff # import our School class
+
+my_school = School("Django School") # create a school instance
+
+# Show link to view all staff and link to view all students
+def index(request):
+    content = { 
+        "school_name": my_school.name
+    }
+    return render(request, "pages/index.html", content)
+
+# Show all staff, and link to staff detail page
+def list_staff(request):
+    content = {
+        "staff_list": my_school.staff,
+    }
+    return render(request, "pages/list_staff.html", content)
+
+# Show all the staff data fields on page
+def staff_detail(request, employee_id):
+    employee = my_school.find_staff_by_id(employee_id)
+    content = {
+        "employee": employee, 
+    }
+    return render(request, "pages/staff_detail.html", content)
+
+# list all students and link to students detail
+def list_students(request):
+    content = {
+        "student_list": my_school.students,
+    }
+    return render(request, "pages/list_students.html", content)
+
+# Show all student data fields
+def student_detail(request, student_id):
+    student = my_school.find_student_by_id(student_id)
+    content = {
+        "student": student,
+    }
+    return render(request, "pages/student_detail.html", content)
